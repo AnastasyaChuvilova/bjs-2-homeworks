90093478b55cf92c90b108b10c101168dec99920
Anastasia Chuvilova <achuvilova098@gmail.com>
2025-01-15 00:43:13 +0500

    Реализованы функции к ДЗ лекции 4 "Объекты"

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
-function Student(name, gender, age) {
-  
+function Student(studentName, studentGender, studentAge) {
+	this.name = studentName, this.gender = studentGender, this.age = studentAge, this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...studentMarks) {
+	if (this.hasOwnProperty("marks")) {
+		this.marks.push(...studentMarks);
+	};
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.hasOwnProperty("marks") && this.marks.length != 0) {
+		return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+	} else {
+		return 0;
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
